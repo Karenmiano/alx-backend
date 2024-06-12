7cc1cd0398acc6a3633c8fdd926fb25be0ce5f94
Karenmiano <mianokaren1798@gmail.com>
2024-06-12 23:35:59 +0300

    add queue

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,17 @@
+import kue from "kue";
+
+const queue = kue.createQueue();
+const jobData = { phoneNumber: "0708552625", message: "Hello" };
+const job = queue.create("push_notification_code", jobData).save((err) => {
+  if (!err) {
+    console.log(`Notication job created: ${job.id}`);
+  }
+});
+
+job.on("complete", (result) => {
+  console.log("Notification job completed");
+});
+
+job.on("failed", (errorMessage) => {
+  console.log("Notification job failed");
+});
